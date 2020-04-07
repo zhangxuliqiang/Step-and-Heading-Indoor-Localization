97b845e142ef99b4a1fe4939c4a2359a2129e8da
Bart <van.ingen.bart@gmail.com>
2020-04-07 12:28:09 +0200

    add json dataset

--- utils/setup_data_sets.m
+++ utils/setup_data_sets.m
@@ -4,14 +4,28 @@ file.directory = '/home/vaningen/MEGAsync/MSc Sensor Fusion Thesis/Code and Data
 file.name = 'HIMU-2020-03-24_18-58-04.csv';
 hyperIMU_inhand.file = file;
 hyperIMU_inhand.dataSetProp = DataSetProp("Accelerometer","timestamp",1/1000, ...
-                          "K6DS3TR_Accelerometerx", ...
-                          "K6DS3TR_Accelerometery", ...
-                          "K6DS3TR_Accelerometerz");
+                          "X","Y","Z");
 
 %%
+clear user1_armband
 
 file.directory = '/home/vaningen/MEGAsync/MSc Sensor Fusion Thesis/Code and Datasets/SHS Code/datasets/step counting/';
 file.name = 'user1_armband_1506423438471.csv';
 user1_armband.file = file;
 user1_armband.dataSetProp = DataSetProp("Accelerometer","timestamp",(1E-9), ...
-    "Accelerometerx","Accelerometery","Accelerometerz")
+    "X","Y","Z");
+
+%%
+clear person1_test_path1
+
+file.directory = '/home/vaningen/MEGAsync/MSc Sensor Fusion Thesis/Code and Datasets/SHS Code/datasets/step length/person01';
+file.name = '/test_path1/pocket_position_normal_walking_speed.json';
+person1_test_path1.file = file;
+person1_test_path1.dataSetProp = DataSetProp("Accelerometer","timestamp",(1E-2), ...
+    "X","Y","Z");
+
+
+
+
+
+
