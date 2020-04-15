5e48be6d83ae5eb01ec516d312dafbf48f8e7ea5
Bart <van.ingen.bart@gmail.com>
2020-04-15 16:11:34 +0200

    change step_detection name to sd_components

--- step detection and counting/stepDetection.m
+++ step detection and counting/stepDetection.m
@@ -19,7 +19,7 @@ disp(['dataset size is:' int2str(height(Acceleration)) ' rows']);
 debugDisp('     import data',debug)
 
 %% create timetable for processed data
-step_detection = timetable(Acceleration.Time);
+sd_components = timetable(Acceleration.Time);
 
 %% optimal settings according to salvi et al
 
@@ -43,28 +43,28 @@ pp_window_t = 0.200; %s
 %% preprocessing: generate the norm of the acceleration signal
 % TODO: interpolation to get constant sampling time
 debugDisp('     calculate norm',debug)
-step_detection.acc0_magnitude = sqrt(Acceleration.X.^2 + Acceleration.Y.^2 + ...
+sd_components.acc0_magnitude = sqrt(Acceleration.X.^2 + Acceleration.Y.^2 + ...
     Acceleration.Z.^2);
 
 %% Threshold on standard deviation of acceleration magnitude
 debugDisp('     apply threshold on standard deviation',debug)
-step_detection.acc0_magnitude_thres = NaN(height(step_detection),1);
-step_detection.acc0_magnitude_std = NaN(height(step_detection),1);
+sd_components.acc0_magnitude_thres = NaN(height(sd_components),1);
+sd_components.acc0_magnitude_std = NaN(height(sd_components),1);
 
-step_detection.acc0_magnitude_std = movstd(step_detection.acc0_magnitude,70);
+sd_components.acc0_magnitude_std = movstd(sd_components.acc0_magnitude,70);
 
-threshold_row_index = step_detection.acc0_magnitude_std > 0.6;
-threshold_rows = step_detection(threshold_row_index,:);
+threshold_row_index = sd_components.acc0_magnitude_std > 0.6;
+threshold_rows = sd_components(threshold_row_index,:);
 
-step_detection.acc0_magnitude_thres = step_detection.acc0_magnitude .* threshold_row_index;
+sd_components.acc0_magnitude_thres = sd_components.acc0_magnitude .* threshold_row_index;
 
 %% Filter Convolution process
 debugDisp('     Apply gaussian filter',debug)
 gauss_window = gaussianWindow(filt_window_size, filt_std);
 gauss_sum = sum(gauss_window);
 kernel = transpose(gauss_window./gauss_sum);
-conv_gauss_filter = conv(step_detection.acc0_magnitude_thres, kernel, 'same');
-step_detection.acc1_conv_gauss = conv_gauss_filter;
+conv_gauss_filter = conv(sd_components.acc0_magnitude_thres, kernel, 'same');
+sd_components.acc1_conv_gauss = conv_gauss_filter;
 
 %% Scor convolution process
 debugDisp('     Apply scoring',debug)
@@ -72,32 +72,32 @@ score_middle_index = round(score_window_size/2);
 
 kernel1 = ones(score_window_size,1).*(-1/(score_window_size - 1));
 kernel1(score_middle_index,1) = 1;
-conv_score = conv(step_detection.acc1_conv_gauss, kernel1, 'same');
+conv_score = conv(sd_components.acc1_conv_gauss, kernel1, 'same');
 
-step_detection.acc2_conv_score = conv_score;
+sd_components.acc2_conv_score = conv_score;
 
 %% Detection stage:
 debugDisp('     Detect peaks',debug)
 % Detecting outliers with builtin matlab methods
-step_detection.acc3_quick_detect = NaN(height(step_detection),1);
+sd_components.acc3_quick_detect = NaN(height(sd_components),1);
 
-cum_moving_mean = movmean(step_detection.acc2_conv_score, [length(step_detection.acc2_conv_score)-1 0]);
-cum_moving_std = movstd(step_detection.acc2_conv_score, [length(step_detection.acc2_conv_score)-1 0]);
+cum_moving_mean = movmean(sd_components.acc2_conv_score, [length(sd_components.acc2_conv_score)-1 0]);
+cum_moving_std = movstd(sd_components.acc2_conv_score, [length(sd_components.acc2_conv_score)-1 0]);
 
-cum_detect_score_row_index = (step_detection.acc2_conv_score - cum_moving_mean) > ...
+cum_detect_score_row_index = (sd_components.acc2_conv_score - cum_moving_mean) > ...
                     cum_moving_std .* detect_threshold;
                 
-threshold_rows = step_detection(cum_detect_score_row_index,:);
-step_detection(threshold_rows.Time,:).acc3_quick_detect = threshold_rows.acc1_conv_gauss;
+threshold_rows = sd_components(cum_detect_score_row_index,:);
+sd_components(threshold_rows.Time,:).acc3_quick_detect = threshold_rows.acc1_conv_gauss;
 
 %% find local maxima through sliding window
 debugDisp('     Find local maxima',debug)
-step_detection.acc4_builtin_max = NaN(height(step_detection),1);
+sd_components.acc4_builtin_max = NaN(height(sd_components),1);
 
 builtinmax = islocalmax(sd_components.acc3_quick_detect,'MinSeparation',seconds(pp_window_t), ...
                         'SamplePoints',sd_components.Time);
 
 local_max_rows = sd_components(builtinmax,:);
 sd_components(local_max_rows.Time,:).acc4_builtin_max = local_max_rows.acc3_quick_detect;
-step_detection(local_max_rows.Time,:).acc4_builtin_max = local_max_rows.acc3_quick_detect;
+
 
