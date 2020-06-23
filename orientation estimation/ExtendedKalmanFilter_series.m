c08c1b2aaf14328ad9ad5ce1d7ef5fb017f8225c
Bart <van.ingen.bart@gmail.com>
2020-06-23 16:23:28 +0200

    formatting

--- orientation estimation/ExtendedKalmanFilter_series.m
+++ orientation estimation/ExtendedKalmanFilter_series.m
@@ -13,24 +13,14 @@ P = 0.01 * eye(4);
 
 calAcc_R = 0.012 * eye(3);
 
-calGyr_R = 0.0033^2 * eye(3);
+calGyr_R = 0.0033 * eye(3);
 
-calMag_R = 0.005 * eye(3);
+calMag_R = 0.01 * eye(3);
 
-% acc = accSampled{:,:}';
-% gyro = gyrSampled{:,:}'; 
-% mag = magSampled{:,:}';
 combined = [combined_raw.X, combined_raw.Y, combined_raw.Z]';
 type = [combined_raw.Type];
 
-% gyroscope time update
-
 dT = [0; seconds(diff(gyrSampled.Time))];
-
-% transpose measurements to get column vectors for matrix operations
-% gyro = [data.gyr_X, data.gyr_Y, data.gyr_Z]';
-% acc = [data.acc_X, data.acc_Y, data.acc_Z]';
-% mag = [data.mag_X, data.mag_Y, data.mag_Z]';
 Time = [seconds(combined_raw.Time)];
 
     
@@ -43,12 +33,7 @@ estimate = repmat(struct('Time', nan, ...
 
 g = [0; 0; -9.81];
 
-% dip_angle = 67.095;
-% mag_field = [cosd(dip_angle);sind(dip_angle); 0 ];
-% mag_field =  magnetic.vector./norm(magnetic.vector);
-
 mag_vector = mean(magnetic{:,:});
-% mag_vector(3) = 0;
 mag_field =  transpose(mag_vector./norm(mag_vector));
 
 counter = 0;
@@ -56,7 +41,7 @@ counter = 0;
 for index = 1:1:height(combined_raw)
     
     if mod(round(index/height(combined_raw),2)*100,10) == 0  && ...
-            round(index/height(combined_raw))~= 0
+            round(index/height(combined_raw)*100) ~= 0
         disp(['percentage complete: ' num2str(round(index/height(combined_raw),2)*100)])
     end
     
@@ -78,30 +63,7 @@ for index = 1:1:height(combined_raw)
                 x.prior_est = est;
             end
     % -------------- MEASUREMENT UPDATES ------------------
-        case Types(3)
-            dRdq_mag = dRqdq(est);
-            H_mag = [dRdq_mag(:,:,1)'*mag_field, ...
-                     dRdq_mag(:,:,2)'*mag_field, ...
-                     dRdq_mag(:,:,3)'*mag_field, ...
-                     dRdq_mag(:,:,4)'*mag_field];
-
-            H = [ H_mag];
-
-            y_hat = [ quat2rotmat(est)' * mag_field];
-
-            R = [ calMag_R];
-
-            error = y - y_hat;
-
-            S = H*P*H' + R;
-            K = (P*H') / S;
-            P = P - K*S*K';
-            est = est + K* error;
-
-            est  = est/norm(est);
-        %     est = est * sign(est(1));
-            J = (1/norm(est)^3)*(est*est');
-%             P = J*P*J';
+       
             
         case Types(2)
             % Accelerometer measurement update
@@ -116,6 +78,31 @@ for index = 1:1:height(combined_raw)
             y_hat = [-quat2rotmat(est)' * g];
 
             R = [calAcc_R];
+
+            error = y - y_hat;
+
+            S = H*P*H' + R;
+            K = (P*H') / S;
+            P = P - K*S*K';
+            est = est + K* error;
+
+            est  = est/norm(est);
+        %     est = est * sign(est(1));
+            J = (1/norm(est)^3)*(est*est');
+%             P = J*P*J';
+
+         case Types(3)
+            dRdq_mag = dRqdq(est);
+            H_mag = [dRdq_mag(:,:,1)'*mag_field, ...
+                     dRdq_mag(:,:,2)'*mag_field, ...
+                     dRdq_mag(:,:,3)'*mag_field, ...
+                     dRdq_mag(:,:,4)'*mag_field];
+
+            H = [ H_mag];
+
+            y_hat = [ quat2rotmat(est)' * mag_field];
+
+            R = [ calMag_R];
 
             error = y - y_hat;
 
