86509d4b213277c20208dec60b74669ed6f509b4
Bart <van.ingen.bart@gmail.com>
2020-06-23 11:59:17 +0200

    formatting particle filter and using a delta angle

--- particle filter/particle_filter.m
+++ particle filter/particle_filter.m
@@ -2,37 +2,55 @@
 %% generating particle filter map
 
 % defining the scale of the map
-image = imread('pietheinstraat.png');
+image = imread('pietheinstraatv2.png');
 I = rgb2gray(image);
 white = I < 250;
 
+[row_size, column_size] = size(white);
+
+Red = image(:,:,2)<10;
+
+[row,col] = find(Red);
+
+start_point = [row_size - mean(row),mean(col)];
+
+white(row,col) = 0;
+
 fig = figure(1);
-imshow(image)
+imshow(white)
+
+
+
 hold on
-scale_points = ginput(2);
+% scale_points = ginput(2);
 hold off
 close(fig)
-
+% 
 points_per_meter = diff(scale_points(:,1))/20;
 
+points_per_meter = 2.65;
 map = occupancyMap(white,points_per_meter);
-
+start_point_meter = start_point/points_per_meter;
+%%
 figure()
-show(map)
 hold on
-start_point_meter = ginput(1);
-
+show(map)
+scatter(start_point_meter(2),start_point_meter(1),'bx')
+hold off
 
 %% distribute particles over map
 
 nr_particles = 100;
 particle_lists = {};
 
+delta_angle = [0; diff(step_orient.yaw)];
+
 % particle initilization
 
-particle_list = repmat(struct('x', start_point_meter(1), ...
-        'y',start_point_meter(2)), 'yaw', step_orient(1,:).yaw, nr_particles, 1 );
+particle_list = repmat(struct('x', start_point_meter(2), ...
+        'y',start_point_meter(1), 'yaw', step_orient(1,:).yaw), nr_particles, 1 );
 
+    
 for timestep = 1: height(step_orient)
     
     disp(['pf completed:' num2str(timestep/height(step_orient))])
@@ -49,22 +67,23 @@ for timestep = 1: height(step_orient)
  
     % time update
         
-    sl_noise = random('Normal', 0, 0.1, nr_particles, 1);
+    sl_noise = random('Normal', 0.25, 0.2, nr_particles, 1);
     orientation_noise = random('Normal', 0, 0.4, nr_particles, 1);
 
-    sl_realization = shs.steps.data.step_length(timestep)+0.25 + sl_noise;
-    orient_realization = step_orient(timestep,:).yaw + orientation_noise;
+    sl_realization = shs.steps.data.step_length(timestep) + sl_noise;
+    orient_realization = [particle_list.yaw]'+ delta_angle(timestep) + orientation_noise;
 
-    temp_x = num2cell([particle_list.x]' + cos(orient_realization).* sl_realization);
-    [particle_list.x] = temp_x{:};
-
-    temp_y = num2cell([particle_list.y]' + ...
-                         sin(orient_realization).* sl_realization');
+    temp_angle = num2cell(orient_realization);    
     
+    temp_x = num2cell([particle_list.x]' + cos(orient_realization).* sl_realization);
+    
+    temp_y = num2cell([particle_list.y]' + sin(orient_realization).* sl_realization');
+    
+    [particle_list.yaw] = temp_angle{:};
+    [particle_list.x] = temp_x{:};
     [particle_list.y] = temp_y{:};
     
-    particle_lists(timestep,:) = {particle_list};
-    
+    particle_lists(timestep,:) = {particle_list};  
 
     
 end
