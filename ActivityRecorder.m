de1bd304f172de4567e6b54b3fcf431605bfef8c
Bart <van.ingen.bart@gmail.com>
2020-08-10 09:32:54 +0200

    add activity recorder

---
+++ ActivityRecorder.m
@@ -0,0 +1,20 @@
+
+
+global data
+global i
+i= 0;
+
+data = [];
+h_fig = figure;
+set(h_fig,'KeyPressFcn',@myfun);
+
+function myfun(src,event)
+    global data
+    global i
+    i = i + 1;
+   disp(event.Key);
+   data(i,1)=now;
+   disp(data(i,1))
+   data(i,2)=event.Key;
+
+end
