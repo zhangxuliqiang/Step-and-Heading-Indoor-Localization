a909fdb328d7d97de19e002975b9e1a68607c759
Bart <van.ingen.bart@gmail.com>
2020-08-17 11:06:03 +0200

    use systematic resampling and weighted measurement update

--- particle filter/ParticleFilterSysResample.m
+++ particle filter/ParticleFilterSysResample.m
@@ -10,35 +10,23 @@ y_pos = start_point_meter(1) * ones(nr_particles,1);
 yaw = step_orient(1,:).yaw * ones(nr_particles,1);
 weight = 1/nr_particles * ones(nr_particles,1);
 
- particle_list = table(x_pos, y_pos, yaw, weight);
-% particle_list = repmat(struct('x_pos', start_point_meter(2), ...
-%                               'y_pos',start_point_meter(1), ...
-%                               'yaw', step_orient(1,:).yaw, ...
-%                               'weight', 1/nr_particles), ...
-%                               nr_particles, 1 );
+particle_list = table(x_pos, y_pos, yaw, weight);
 
 output = [];
 
 for timestep = 1: height(step_orient)
     
-       
+    particle_positions = [particle_list.x_pos,particle_list.y_pos];
+    
     %# measurement update
     % first check whether the particles are outside passable region
-    invalid_points = logical(checkOccupancy(map,[particle_list.x_pos, particle_list.y_pos]));
-    normalization_weight = sum(particle_list.weight.* ~invalid_points);
-    
-    if isnan(normalization_weight) || normalization_weight == 0
-        error('normalization is nan or 0')
-    end
-    
-    % if there are no valid points break, and continue to next
-    % itteration
-    
-    if(normalization_weight == 0 )
+    invalid_points = logical(checkOccupancy(map,particle_positions));
+        
+    if(sum(~invalid_points) == 0 )
         break
     end
     
-    particle_list.weight = (1/normalization_weight) * particle_list.weight.* ~invalid_points;
+    particle_list = MeasUpdate(particle_list, ~invalid_points);
     
 %     if sum((gps_tol.Time - step_orient(timestep,:).Time) < 0.01) == 1
 %         dist = 1;
@@ -62,14 +50,8 @@ for timestep = 1: height(step_orient)
             j=j+1;
         end
         
-        if j > N
-            error('j is larger than N')
-        end
-    end
-
-particle_list = particle_list(resample_index,:);
+    particle_list = Resample(particle_list);
     
-         
     % time update
     
     sl_noise = random('Normal', 0.20, std_sl, height(particle_list), 1);
@@ -82,9 +64,29 @@ particle_list = particle_list(resample_index,:);
     particle_list.x_pos = particle_list.x_pos + cos(orient_realization).* sl_realization;
     
     particle_list.y_pos = particle_list.y_pos + sin(orient_realization).* sl_realization;
-        
+    
     output(timestep,:).Time = step_orient(timestep,:).Time;
     output(timestep,:).particle_lists = particle_list;
 end
 final_timestep = timestep;
+end
+
+function particle_list = Resample(particle_list) % Multinomial sampling  with sort 
+
+N = height(particle_list);
+
+u=([0:N-1]'+(rand(N,1)))/N;
+wc=cumsum(particle_list.weight) ; 
+wc=wc/wc(N) ; 
+[dum,ind1]=sort([u;wc]); 
+ind2=find(ind1<=N); 
+resample_index = ind2-(0:N-1)'; 
+
+particle_list = particle_list(resample_index,:);
+particle_list.weight = ones(N,1)/N;
+end
+
+function particle_list = MeasUpdate(particle_list, post_prob)
+    normalization_weight = sum(particle_list.weight.*post_prob);
+    particle_list.weight = (1/normalization_weight) * particle_list.weight.* post_prob;
 end
