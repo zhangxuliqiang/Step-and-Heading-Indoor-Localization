ae27f1b3b60f45c88ac21a954ea7d4de85fc456a
Bart <van.ingen.bart@gmail.com>
2020-09-27 14:30:11 +0200

    formatting findSteps

--- step detection and counting/findSteps.m
+++ step detection and counting/findSteps.m
@@ -1,8 +1,9 @@
-function result = findSteps(target, target_struct)
-% find the number of step taken according to the ground truth
+function steps = findSteps(target, target_timetable)
+% find the index of step taken and find the raw data that corresponds to it
 step_index = find(target);
-steps.data = target_struct(step_index,:);
+steps.data = target_timetable(step_index,:);
+
+% find the total amount steps taken
 [steps.nr_steps, ~] = size(step_index);
 
-result = steps;
 end
