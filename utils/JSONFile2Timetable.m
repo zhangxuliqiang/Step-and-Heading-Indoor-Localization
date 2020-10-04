7cb0c8c67bd90229ac72d514a14c50c1f43a5d56
Bart <van.ingen.bart@gmail.com>
2020-10-04 10:59:07 +0200

    add orientation for json import and rename variables

--- utils/JSONFile2Timetable.m
+++ utils/JSONFile2Timetable.m
@@ -1,4 +1,4 @@
-function [Data, elapsed] = JSONFile2Timetable(dataset)
+function [properties,raw_data] = JSONFile2Timetable(dataset)
 
 file = dataset.file;
 dataSetProp = dataset.dataSetProp;
@@ -6,21 +6,28 @@ file_path = strcat(file.directory, file.name);
 
 target = fileread(file_path);
 
-step_length_data = jsondecode(target);
+sample_data = jsondecode(target);
 %%
 delta_t = dataset.dataSetProp.time_units;
 
-elapsed_time  = transpose(0: delta_t: (length(step_length_data.linear_acceleration.x)-1).*delta_t);
+elapsed_time  = transpose(0: delta_t: (length(sample_data.linear_acceleration.x)-1).*delta_t);
 data_array = [elapsed_time, ...
-        step_length_data.linear_acceleration.x,...
-        step_length_data.linear_acceleration.y ...
-        step_length_data.linear_acceleration.z];
+        sample_data.linear_acceleration.x,...
+        sample_data.linear_acceleration.y,...
+        sample_data.linear_acceleration.z,...
+        sample_data.orientation.x,...
+        sample_data.orientation.y ...
+        sample_data.orientation.z];
     
 VariableNames = [ dataSetProp.time_var_name, dataSetProp.column_names];
  
-Data = array2table(data_array,'VariableNames', VariableNames);
-Data.Time = seconds(Data.Time);
-Data = table2timetable(Data);
-Data = unique(Data);
+raw_data = array2table(data_array,'VariableNames', VariableNames);
+raw_data.Time = seconds(raw_data.Time);
+raw_data = table2timetable(raw_data);
+raw_data = unique(raw_data);
+
+properties = sample_data;
+fields = {'linear_acceleration','orientation'};
+properties = rmfield(properties,fields);
 
 end
