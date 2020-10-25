e94fee6b6766140d457b5aef9959cae722cd39b2
bart <van.ingen.bart@gmail.com>
2020-10-25 18:09:11 +0100

    compare to ground working with current setup

--- particle filter/CompareToGroundTruth.m
+++ particle filter/CompareToGroundTruth.m
@@ -1,19 +1,14 @@
 function [mean_error, mean_std_error] = CompareToGroundTruth(specific_pf, utils, gt_walkingroute)
 
-pf_door_open_index = find(specific_pf.door_detect);
 
-pf_door_open = specific_pf(pf_door_open_index,:); 
-
-pf_dist_error = [];
-
-for i = 1:height(pf_door_open)
-    particle_list = pf_door_open(i,:).particle_lists{1,1};
+for i = 1:height(specific_pf)
+    particle_list = specific_pf(i,:).particle_lists{1,1};
     pf_x_positions = particle_list(:,utils.index.x_pos);
     pf_y_positions = particle_list(:,utils.index.y_pos);
-    gt_point = gt_walkingroute(i,:); 
+    gt_point = gt_walkingroute(specific_pf(i,:).Time,:);
     
-    x_pos_error = pf_x_positions - gt_point.x_pos;
-    y_pos_error = pf_y_positions - gt_point.y_pos;
+    x_pos_error = pf_x_positions - gt_point.x;
+    y_pos_error = pf_y_positions - gt_point.y;
     
     error_vec = [x_pos_error'; y_pos_error'];
     
