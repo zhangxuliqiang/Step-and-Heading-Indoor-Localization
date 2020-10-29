b9e25ccd78b5ec49964c73ef7b5fd3a6a361c64e
Bart <van.ingen.bart@gmail.com>
2020-10-29 12:12:13 +0100

    rewrite comparetoground truth to use new ground truth format

--- particle filter/CompareToGroundTruth.m
+++ particle filter/CompareToGroundTruth.m
@@ -1,31 +1,39 @@
-function [mean_error, mean_std_error] = CompareToGroundTruth(specific_pf, utils, gt_walkingroute)
+function [distance_rmse,mean_traj] = CompareToGroundTruth(shs,specific_pf, utils, position_data)
 
+all_bt_trajectories_x = [];
+all_bt_trajectories_y = [];
+for ancestor = 1:length(specific_pf.particle_lists{1,1})
+    backtrack_trajectory = [];
+    for ii = height(specific_pf):-1:1
+        particle_list = specific_pf(ii,:).particle_lists{1,1};
+        backtrack_trajectory = [backtrack_trajectory;particle_list(ancestor,utils.index.x_pos:utils.index.y_pos)];
+        % finding the next ancestor
+        ancestor  = particle_list(ancestor,utils.index.particle_history);
+    end
+    all_bt_trajectories_x = [all_bt_trajectories_x; backtrack_trajectory(:,1)'];
+    all_bt_trajectories_y = [all_bt_trajectories_y; backtrack_trajectory(:,2)'];
+end
 
-for i = 1:height(specific_pf)
-    particle_list = specific_pf(i,:).particle_lists{1,1};
-    pf_x_positions = particle_list(:,utils.index.x_pos);
-    pf_y_positions = particle_list(:,utils.index.y_pos);
-    gt_point = gt_walkingroute(specific_pf(i,:).Time,:);
-    
-    x_pos_error = pf_x_positions - gt_point.x;
-    y_pos_error = pf_y_positions - gt_point.y;
-    
-    error_vec = [x_pos_error'; y_pos_error'];
-    
-    distance_error = vecnorm(error_vec);
-    
-    pf_dist_error(i).Time = specific_pf(i,:).Time;
-    pf_dist_error(i).mean = mean(distance_error);
-    pf_dist_error(i).std = std(distance_error);
-    
-end
-if ~isempty(pf_dist_error)
-    pf_stat.dist_error = struct2table(pf_dist_error);
-    mean_error  = mean(pf_stat.dist_error.mean);
-    mean_std_error = mean(pf_stat.dist_error.std);
-else
-mean_error = nan;
-mean_std_error = nan;
-end
+%%
+mean_bt_traj_x = mean(all_bt_trajectories_x);
+mean_bt_traj_y = mean(all_bt_trajectories_y);
+
+mean_traj_y = mean_bt_traj_y(end:-1:1);
+mean_traj_x = mean_bt_traj_x(end:-1:1);
+mean_traj = [mean_traj_x', mean_traj_y'];
+%%
+
+pos_data = struct2table(position_data);
+ pos_data.time = seconds(pos_data.time);
+ pos_data = table2timetable(pos_data);
+ 
+ pos_data = retime(pos_data, shs.position.time, 'spline');
+ 
+ x_error = pos_data.x - mean_traj_x';
+ y_error = pos_data.y - mean_traj_y';
+ 
+distance_error = sqrt(x_error.^2 + y_error.^2);
+
+distance_rmse = rms(distance_error);
 
 end
