7cf8175aa22d206cd6937024178773abfe8e4ab2
Bart <van.ingen.bart@gmail.com>
2020-10-29 13:11:22 +0100

    variable name changes

--- particle filter/shs_particle_filter_inside.m
+++ particle filter/shs_particle_filter_inside.m
@@ -3,13 +3,13 @@
 
 % defining the scale of the map
 image = imread('binnenkaart (copy).png');
-white_regions_image  = rgb2gray(image)< 250;
+not_white_regions_image  = rgb2gray(image)< 250;
 
-[row_size, column_size] = size(white_regions_image);
+[row_size, column_size] = size(not_white_regions_image);
 
 red_regions_image = image(:,:,1)>250 & image(:,:,3)<50 & image(:,:,2)<50 ;
 
-walls_image = white_regions_image ~= red_regions_image;
+walls_image = not_white_regions_image ~= red_regions_image;
 
 points_per_meter = 90; %<-- this value was generated through pixel to meter
                        %    conversion inside plotter file
@@ -20,7 +20,6 @@ door_pixel_location = bwboundaries(red_regions_image);
 
 doors = grid2local(walls, cell2mat(door_pixel_location));
 
-%%
 
 fig = figure(); 
 show(walls)
@@ -34,12 +33,12 @@ close(fig)
 %% finding completed track
 clc
 
-nr_particles = 300;
+nr_particles = 600;
 
 while true
     tic
     [specific_pf, utils] = ParticleFilter_inside_mat(start_point,nr_particles, ...
-        shs.step_and_orient, 0.2, 0.2, walls, doors, shs_sample.door_handle_use);
+        shs.step_and_orient, 0.2, 0.14, walls, doors, shs_sample.door_handle_use);
     disp(['pf completed:' num2str(utils.final_timestep/height(shs.step_and_orient))])
     toc
     
@@ -49,9 +48,12 @@ while true
 
 end
 
+%%
+[distance_rmse,mean_traj] = CompareToGroundTruth(shs,specific_pf, utils, position_data);
+
 %% Individual testing
 
- [specific_pf, utils] = ParticleFilter_inside_mat(start_point,nr_particles, ...
+ [specific_pf,particle_history, utils] = ParticleFilter_inside_mat(start_point,nr_particles, ...
          shs.step_and_orient, 0.3, 0.1, walls, doors, shs_sample.door_handle_use);
  disp(['pf completed:' num2str(utils.final_timestep/height( shs.step_and_orient))])
     %%
@@ -60,12 +62,12 @@ pf_stat = CompareToGroundTruth(specific_pf,utils, pos_data)
 
 %%
 clc
-nr_particles = 100;
+nr_particles = 400;
 std_orient_counter = 0;
 
 orient_pf = [];
 
-for std_orient = 0.05:0.05:0.3
+for std_orient = 0.1:0.02:0.2
     std_orient_counter = std_orient_counter +1;
     
     fprintf('std_orient = %f \n', std_orient )
@@ -73,12 +75,12 @@ for std_orient = 0.05:0.05:0.3
     sl_pf = [];
     sl_std_counter = 0;
     
-    for std_sl = 0.05:0.05:0.3
+    for std_sl = 0.1:0.02:0.2
         sl_std_counter = sl_std_counter +1;
         fprintf('       std_sl = %f \n', std_sl )
         realizations =[];
         
-        for itteration = 1:10
+        for itteration = 1:5
             fprintf('       itteration: %i' ,itteration )
             
 % [output, utils] = ParticleFilter_inside_mat(start_point, nr_particles,  shs.step_and_orient, std_sl, std_orient, walls, doors, door_handle_use)           
@@ -88,8 +90,11 @@ for std_orient = 0.05:0.05:0.3
             
             realizations(itteration).percent_complete = utils.final_timestep/height( shs.step_and_orient);
             
-            [realizations(itteration).mean_error, realizations(itteration).mean_std_error] =  ...
-                CompareToGroundTruth(specific_pf,utils, pos_data);
+            if realizations(itteration).percent_complete == 1
+                realizations(itteration).distance_rmse = CompareToGroundTruth(shs,specific_pf, utils, position_data);
+            else
+                realizations(itteration).distance_rmse = nan; 
+            end
         end
         sl_pf(sl_std_counter).std_sl = std_sl;
         sl_pf(sl_std_counter).realizations = realizations;
@@ -98,12 +103,12 @@ for std_orient = 0.05:0.05:0.3
     orient_pf(std_orient_counter).std_orient = std_orient;
     orient_pf(std_orient_counter).sl_pf = sl_pf;
 end
-save(orient_pf)
+save('orient_pf')
 %%
 
 plot_index = 1;
 sub_plot_length = length(orient_pf) + 1;
-std_sl_x_axis  = 0.05:0.05:0.3; 
+std_sl_x_axis  = 0.1:0.02:0.2; 
 
  t = tiledlayout(length(orient_pf),1);
  
@@ -111,17 +116,16 @@ for i = 1:length(orient_pf)
     
     specific_sl_pf = orient_pf(i).sl_pf;
     
-    bc_mean_error = [];
+    rmse = [];
     bc_mean_std = [];
     
     for std_sl_index = 1:length(specific_sl_pf)
         track_completed_index = [specific_sl_pf(std_sl_index).realizations.percent_complete] == 1;
-        bc_mean_error = [bc_mean_error; [specific_sl_pf(std_sl_index).realizations.mean_error].*track_completed_index];
-        bc_mean_std = [bc_mean_std; [specific_sl_pf(std_sl_index).realizations.mean_std_error].*track_completed_index];
+       rmse = [rmse; specific_sl_pf(std_sl_index).realizations.distance_rmse];       
     end
     
     ax(i) = nexttile;
-    b = bar(ax(i),std_sl_x_axis,bc_mean_error);
+    b = bar(ax(i),std_sl_x_axis,rmse);
     title(['orientation std: '  num2str(orient_pf(i).std_orient)])
     set(ax(i),'fontsize',10)
     ylim([0,10])
