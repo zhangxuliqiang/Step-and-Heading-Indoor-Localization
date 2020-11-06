a50bbb7246a880ebc105953a7ac9557279f0cc5d
Bart <van.ingen.bart@gmail.com>
2020-11-06 10:41:55 +0100

    add function input to plot or not

--- utils/plotTrajectory.m
+++ utils/plotTrajectory.m
@@ -1,7 +1,10 @@
-function [positions,step_orient] = plotTrajectory(orient_estimate,shs, door_handle_use)
+function [positions,step_orient] = plotTrajectory(orient_estimate,shs, door_handle_use,plot_flag)
 
-if nargin<3
+if nargin<4
   door_handle_use = [];
+  plot_flag = [];
+elseif nargin < 3
+  plot_flag = [];
 end
 
 euler_angles = timetable(orient_estimate.Time);
@@ -14,7 +17,7 @@ euler_angles.yaw = euler_angles.yaw +pi/2;
 %%
 temp_data = euler_angles(shs.steps.data.Time,:);
 
-[step_time,ia,~] = unique(temp_data.Time,'first');
+[~,ia,~] = unique(temp_data.Time,'first');
 
 step_orient = temp_data(ia,:);
 
@@ -51,27 +54,29 @@ if ~isempty(door_handle_use)
 end
 
 % plot the trajectory
-figure()
-x = [positions.x]';
-y = [positions.y]';
-z = [seconds(positions.time)]';
-col = z;  % This is the color, vary with x in this case.
-hold on
-surface([x;x],[y;y],[z;z],[col;col],...
+
+if isempty(plot_flag)
+    figure()
+    x = [positions.x]';
+    y = [positions.y]';
+    z = [seconds(positions.time)]';
+    col = z;  % This is the color, vary with x in this case.
+    hold on
+    surface([x;x],[y;y],[z;z],[col;col],...
         'facecol','no',...
         'edgecol','interp',...
         'linew',2);
-hcb = colorbar;
-
-if ~isempty(door_handle_use)
-    scatter3(door_detections.x, door_detections.y, seconds(door_detections.time))
+    hcb = colorbar;
+    
+    if ~isempty(door_handle_use)
+        scatter3(door_detections.x, door_detections.y, seconds(door_detections.time))
+    end
+    
+    title(hcb,'Time (sec)')
+    xlabel('X position from initial (meters)')
+    ylabel('Y position from initial (meters)')
+    % title('step and heading system walking around the block')
+    % ylim([-50, 150])
+    % xlim([-150,50])
 end
-
-title(hcb,'Time (sec)')
-xlabel('X position from initial (meters)')
-ylabel('Y position from initial (meters)')
-title('step and heading system walking around the block')
-% ylim([-50, 150])
-% xlim([-150,50])
-
 end
